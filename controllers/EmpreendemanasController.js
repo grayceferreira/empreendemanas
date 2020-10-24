b7767812caf4ee2580e5a5754c2b353b9fa6c3a6
marinamare <marinamaredesign@gmail.com>
2020-10-24 16:32:43 -0300

    adição do tratamento de erro try/catch nas funções update e remove do EmpreendemanasController

--- controllers/EmpreendemanasController.js
+++ controllers/EmpreendemanasController.js
@@ -1,6 +1,6 @@
 const { connect } = require('../models/Repository')
 const empreendemanasModel = require('../models/EmpreendemanasSchema')
-const { projetosModel } = require('../models/ProjetosSchema')
+const  { projetosModel }  = require('../models/ProjetosSchema')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const SEGREDO = 'MIICXAIBAAKBgQCOl54HaBM/WiL/jPPdFGjm9f8VprUst1J+vs7G/YRGRHYLGqt+M/ljAhcROPy3FdaVi2smqqyZhf4d+EZ9lKM6LVed91sxvcyMFEp6x8R2KS9wIzUtJ6r1MAIKd8HURmbaN4V2TV/FLeOUANRCZ+QhYEy+eNbuVIJANYtXBUSn8QIDAQABAoGBAIuVS/MAJGdNuxjiSA5Q3mfIw03UhWIiirTb39rXbNbESbGRB/NguW38K8yGNoya6hY2BkwxowgeLKX11js0d5sSHgEgL+pDQtXshHu7vlYU0ksHwfmD/R8+ZHJH6F6L0vuzs4NoVK/8iQHFLboUjF2sORyuLHbBmFZQWhInet8pAkEA0OlL2uHCYhkNuokJ9H+OnJEqKS2BtYSkH3Hrh2opZg2HtvUtXEIxzmj/95CzxMXQtNJhQMK3ekvnF3Upcj2avwJBAK67i8OEKM2jerbFKrBqr6/kUkZeyHLA8I4L2C3/3nKPGUj/GAc2xxuK1XxnpC0e3Wqz5OMwzkWU4Ynblsdq2U8CQHu9U6LICbzVHh6YwP7C9xOhoBlXzPZZJGVDssA4j2DVLsednUqCIsIhy0s1uGUazi3sVpJnQwn7H1vzl6ME/j0CQAT7qj+4LCW5LM27j70aPcppW4NQPq0vHW0fn1moe2KO/CydwcSq5kC909rJZeA3ih755GQqRyeq2EfDMGidfncCQD770Za6sJP1/i1vcdoWuWYnhpiU8TNKjFb2vJEN598amcyJV9PlAAdEkszh6EDA76t6/yT6NoUn/y9x4YskzQo='
@@ -76,7 +76,7 @@ const update = (request, response) => {
   const updateEmpreendemana = request.body
   const options = { new: true }
 
-  empreendemanasModel.findByIdAndUpdate(
+  try { empreendemanasModel.findByIdAndUpdate(
     id,
     updateEmpreendemana,
     options,
@@ -91,13 +91,16 @@ const update = (request, response) => {
 
       return response.status(404).send('Ooops! Empreendemana não encontrada.')
     }
-  )
+  )} catch(err){
+    console.log(err)
+    return response.status(404).send({ message: "Empreendemana a ser atualizada não foi encontrada :("})
+  }
 }
 
 const remove = (request, response) => {
   const id = request.params.id
 
-  empreendemanasModel.findByIdAndDelete(id, (error, empreendemana) => {
+  try {empreendemanasModel.findByIdAndDelete(id, (error, empreendemana) => {
     if (error) {
       return response.status(500).send(error)
     }
@@ -107,7 +110,10 @@ const remove = (request, response) => {
     }
 
     return response.status(404).send('Ooops! Não encontramos essa empreendemana.')
-  })
+  })} catch (err) {
+    console.log(err)
+    return response.status(404).send({ message: "Empreendemana a ser excluída não foi encontrada :("})
+  }
 }
 
 
